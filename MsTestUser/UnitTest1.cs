93728bed90e7bd6d72059107e3fcf271ddd68bad
shubhampachghare <shubhampachghare@gmail.com>
2022-12-23 09:58:25 +0530

    [Shubham]-Valid Last name start with cap and 3 char

--- MsTestUser/UnitTest1.cs
+++ MsTestUser/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MsTestUserRegistration;
 
 namespace MsTestUser
@@ -7,12 +8,21 @@ namespace MsTestUser
     public class UnitTest1
     {
         string firstName = "^[A-Z]{1}[a-z]{2,10}$";
+        string lastName = "^[A-Z]{1}[a-z]{2,10}$";
         UserDetails us = new UserDetails();
         [TestMethod]
-        public void TestMethod1()
+        public void FirstName()
         {
             var result = us.FirstName("Shubham", firstName);
             Assert.AreEqual(true, result.Success);
         }
-    }
+        //UC2 for last name
+        [TestMethod]
+        public void LastName()
+        {
+            var result = us.LastName("Pachghare", lastName);
+        Assert.AreEqual(true, result.Success);
+
+        }
+}
 }
--- UserDetails.cs
+++ UserDetails.cs
@@ -10,10 +10,18 @@ namespace MsTestUserRegistration
     public class UserDetails
     {
         public String firstName;
+        public String lastName;
         public Match FirstName(string firstname, string pattern)
         {
             this.firstName = firstname;
             return Regex.Match(firstName, pattern);
+
+        }
+        public Match LastName(string lastname, string pattern)
+        {
+            this.lastName = lastname;
+
+            return Regex.Match(lastName, pattern);
 
         }
     }
